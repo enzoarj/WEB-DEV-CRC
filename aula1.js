85bdd8d0c5a63ea488acc2889b890ef24fe45d46
julia <juliavcst@gamil.com>
2023-02-20 12:15:00 -0300

    exs aula 1

---
+++ aula1.js
@@ -0,0 +1,37 @@
+/*Exercicio 1 - nome, idade, estado civil
+let nome = "Julia";
+console.log(nome);
+
+let idade = 18;
+console.log(idade);
+
+let solteira = true;
+console.log(solteira);
+*/
+
+/*Exercicio 2 - media de 2 numeros
+let n1 = 9; 
+let n2 = 10;
+let media;
+
+media = (n1+n2)/2;
+console.log("Nota 1:", n2, "\nNota 2:", n2, "\nMedia:", media);
+*/
+
+/*Exercicio 3 - tabuada do 5 
+for(let i = 0; i <= 10; i++)
+console.log(i*5);
+*/
+
+/*Antecessor e sucessor
+let num = 5;
+console.log(num-1, num+1);
+*/
+
+/*Porcentagem
+let num = 10;
+let porcent; 
+
+porcent = (num - (num * 0.05));
+console.log(porcent);
+*/
