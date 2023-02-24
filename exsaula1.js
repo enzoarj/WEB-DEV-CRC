878ce36e8b980a9ec0f80ff77a9c983a97b8c7ee
julia <juliavcst@gamil.com>
2023-02-24 09:45:26 -0300

    exercicios aula 1

---
+++ exsaula1.js
@@ -0,0 +1,20 @@
+/*1 - troca de valores 
+let a = 2;
+let b = 4;
+let c;
+console.log(a, b);
+
+c = a;
+a = b;
+b = c;
+console.log(a, c);
+*/
+
+/*2 - aumento do salario
+let salario = 1000;
+let aumento;
+let novosal;
+
+novosal = salario + (salario*0.25);
+console.log("Novo salario: ", novosal);
+*/
