d3fc9e6bc203f801acda5f5234145df0bd3eb217
julia <juliavcst@gamil.com>
2023-02-24 11:38:12 -0300

    atividade aula 1

---
+++ aula2.js
@@ -0,0 +1,56 @@
+/*Ex 1
+let cargo = "DIRETOR";
+cargo = cargo.toLowerCase();
+
+switch(cargo)
+{
+    case "comum":
+        console.log(`Sua perissão é de ${cargo}`);
+    break;
+
+    case "gerente":
+        console.log(`Sua perissão é de ${cargo}`);
+    break;
+    
+    case "diretor":
+        console.log(`Sua perissão é de ${cargo}`);
+    break;
+
+    default:
+        console.log("Invalido."); 
+}
+*/
+
+/*Ex 2
+let a = 4;
+let b = 2;
+let op = "/";
+
+switch(op)
+{
+    case "+":
+      soma = a + b;
+        console.log(`Resultado: ${soma}`);
+    break;
+
+    case "-":
+      sub = a - b;
+        console.log(`Resultado: ${sub}`);
+    break;
+
+    case "*":
+        mult = a * b;
+        console.log(`Resultado: ${mult}`);
+    break;
+
+    case "/":
+        div = a / b;
+        console.log(`Resultado: ${div}`);
+    break;
+
+    default:
+        console.log("Operação inválida.");
+}
+*/
+
+//EX 3
